637a800ab0a772e51316c3977dd5a3e480f30546
murali krishna <77238998+muralidpmk@users.noreply.github.com>
2021-06-27 19:01:30 +0530

    Initial Commit

---
+++ notification.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  8 20:31:12 2021
+
+@author: Murali
+"""
+
+
+from plyer import notification
+
+notification.notify(
+    title ="hello",
+    message="May I know your free time...!",
+    app_icon=None,
+    timeout=5)
